4fe0c088e2004664ae70ee87a3b714931edc7cf4
jamesliauw <jamesliauw@163.com>
2015-12-06 10:29:43 +0800

    登录页面
    LOGO居中布局

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,76 +7,55 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-      
-        
+
+    // 登录页面
     .state('login', {
       url: '/page1',
       templateUrl: 'templates/login.html',
       controller: 'loginCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 注册页面
     .state('signup', {
       url: '/page2',
       templateUrl: 'templates/signup.html',
       controller: 'signupCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 商城页面
     .state('shop', {
       url: '/page4',
       templateUrl: 'templates/shop.html',
       controller: 'shopCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 购物车页面
     .state('cart', {
       url: '/page5',
       templateUrl: 'templates/cart.html',
       controller: 'cartCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 用户设置页面
     .state('profile', {
       url: '/page6',
       templateUrl: 'templates/profile.html',
       controller: 'profileCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 订单页面
     .state('order', {
       url: '/page7',
       templateUrl: 'templates/order.html',
       controller: 'orderCtrl'
     })
-        
-      
-    
-      
-        
+
+    // 主页面
     .state('main', {
       url: '/page8',
       templateUrl: 'templates/main.html',
       controller: 'mainCtrl'
     })
-        
-      
     ;
 
   // if none of the above states are matched, use this as the fallback
