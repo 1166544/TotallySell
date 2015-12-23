2d09e2abccdb07dbfb80d401a51011bd250d22d4
jamesliauw <jamesliauw@163.com>
2015-12-23 11:16:05 +0800

    tips
    login页面加入tips处理

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,20 +4,49 @@ angular.module('app.controllers', ['app.services', 'app.filters'])
 .constant("TOP", "top")
 .constant("BOTTOM", "bottom")
 
-// 登录页面控制器
+/**
+ * 登录页面控制器
+ */
 .controller('loginCtrl', function($scope, $state, LoginService)
 {
-    $scope.loginData = {userName:"", password:""};
+    $scope.loginData = {userName:"", password:"", inviated:false};
 
     // 登录处理,成功则跳转至主页面
-    $scope.doLogin = function(){
-      if(LoginService.login($scope.loginData.userName, $scope.loginData.password)){
+    $scope.doLogin = function()
+    {
+      if(LoginService.login($scope.loginData.userName, $scope.loginData.password))
+      {
+        $scope.hideTips();
         $state.go("tab.dash");
+        $scope.clearInput();
       }
+      else{
+        $scope.showTips()
+      }
+    }
+
+    // 移除输入内容
+    $scope.clearInput = function(){
+      $scope.loginData.userName = $scope.loginData.password = "";
+    }
+
+    // 获得焦点处理
+    $scope.doFocus = function(){
+        $scope.hideTips();
+    }
+
+    $scope.showTips = function (){
+      $scope.loginData.inviated = true;
+    }
+
+    $scope.hideTips = function (){
+      $scope.loginData.inviated = false;
     }
 })
 
-// 主页面控制器
+/**
+ * 主页面控制器
+ */
 .controller('DashCtrl', function($scope, DashService, TOP, BOTTOM)
 {
     $scope.topItemData = DashService.getSideItemData(TOP);
