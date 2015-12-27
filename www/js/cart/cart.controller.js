592b20e6cd3f5efbb77edab9da188cf5fb110f80
jamesliauw <jamesliauw@163.com>
2015-12-27 16:25:12 +0800

    catr count
    cart total count

--- www/js/cart/cart.controller.js
+++ www/js/cart/cart.controller.js
@@ -14,6 +14,30 @@
     // 定义购物车列表数据
     $scope.cartDataList = CommonFactory.cartDataList;
 
+    // 总花费
+    $scope.totalCost = 0;
 
+    /**
+     * 转换尺寸简写
+     * @param str
+     * @returns {*|Array.<T>|string|Blob}
+     */
+    $scope.getSizeDesc = function(str){
+      return str.slice(0,1);
+    }
+
+    /**
+     * 计算总花费
+     * @returns {*}
+     */
+    $scope.getTotalCoast = function(){
+      var total;
+      var item;
+      for(var i=0; i < $scope.cartDataList.length; i++){
+        item =$scope.cartDataList[i];
+        total += parseInt(item.pPrice);
+      }
+      return total;
+    }
   }
 })();
