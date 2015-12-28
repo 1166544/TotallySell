f15e4e1025bccfbedeea9b24d36d7688999d7856
jamesliauw <jamesliauw@163.com>
2015-12-28 13:43:02 +0800

    完成Signup页面
    signuup.html

--- www/js/login/signup.controller.js
+++ www/js/login/signup.controller.js
@@ -3,8 +3,47 @@
     .module('app.controllers')
     .controller('signupCtrl', SignupCtrl);
 
-  SignupCtrl.$inject = ["$scope"];
-  function SignupCtrl($scope) {
+  SignupCtrl.$inject = ["$scope", "$state"];
+  /**
+   * 注册新用户逻辑
+   * @param $scope
+   * @constructor
+   */
+  function SignupCtrl($scope, $state) {
 
+    /**
+     * 表单提交数据
+     * @type {{}}
+     */
+    $scope.signUpData = {
+      firstName:"",
+      lastName:"",
+      password:"",
+      email:"",
+      phone:"",
+      address:""
+    };
+
+    /**
+     * 重置操作
+     */
+      $scope.reset = function(){
+        $scope.signUpData = {
+          firstName:"",
+          lastName:"",
+          password:"",
+          email:"",
+          phone:"",
+          address:""
+        };
+      }
+
+    /**
+     * 提交表单操作
+     */
+      $scope.signUp = function(){
+        $state.go("tab.dash");
+        $scope.reset();
+      }
   }
 })();
